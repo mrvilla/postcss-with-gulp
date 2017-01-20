62204bb6d37993d6b389a5121783b4ff9ae5b3d0
Henry Villa <iammrvilla@gmail.com>
2017-01-20 07:12:37 +0000

    adding more plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssnext = require('cssnext'),
-    precss = require('precss');
+    precss = require('precss'),
+    cssgrace = require('cssgrace'),
+    cssnano = require('cssnano');
 
 
 gulp.task('css', function () {
   var processors = [
-      autoprefixer,
-      cssnext,
-      precss
+    autoprefixer,
+    cssnext,
+    precss,
+    cssgrace
   ];
   
   return gulp.src('postcss/*.css')
     .pipe(postcss(processors))
+    .on('error', gutil.log)
     .pipe(gulp.dest('css'));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('**/*.css', ['css']);
+});
